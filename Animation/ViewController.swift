670f7144bfb8689f82d8a8aaa94079b4f143ea84
Namrata Jain <apple@Apples-MacBook-Pro-3.local>
2020-07-09 13:43:32 +0530

    A Change in variable name

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -15,13 +15,13 @@ class ViewController: UIViewController {
     let photoArray = ["Album4Photo0.jpg", "Album1Photo2.jpg", "Album7Photo2.jpg"]
     let photoCategoryArray = ["Sunshine", "Butterfly", "PinkSky"]
     
-    var circle = UIView(frame: CGRect(x: 0, y: 100, width: 40, height: 40))
+    var bubble = UIView(frame: CGRect(x: 0, y: 100, width: 40, height: 40))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(circle)
-        circle.backgroundColor = UIColor(red: 104/255, green: 134/255, blue: 1, alpha: 1.0)
-        circle.layer.cornerRadius = 20
+        self.view.addSubview(bubble)
+        bubble.backgroundColor = UIColor(red: 104/255, green: 134/255, blue: 1, alpha: 1.0)
+        bubble.layer.cornerRadius = 20
         
         self.view.bringSubviewToFront(categoryCollectionView)
     }
@@ -34,25 +34,25 @@ class ViewController: UIViewController {
         let indexpath = IndexPath(item: 0, section: 0)
         if let cell = categoryCollectionView.cellForItem(at: indexpath) as? CategoryCell {
             let labelPosition = cell.frame.origin.x + 20
-            circle.frame.origin.x = labelPosition
-            circle.frame.origin.y = categoryCollectionView.frame.origin.y + 15
+            bubble.frame.origin.x = labelPosition
+            bubble.frame.origin.y = categoryCollectionView.frame.origin.y + 15
             categoryCollectionView.selectItem(at: indexpath, animated: false, scrollPosition: [])
             cell.nameLabel.textColor = UIColor(white: 0, alpha: 1.0)
         }
     }
     
     func liquidAnimate(position: CGFloat) {
-        let midOfNewPostion = (position - self.circle.frame.origin.x)/2
+        let midOfNewPostion = (position - self.bubble.frame.origin.x)/2
         UIView.animateKeyframes(withDuration: 0.6, delay: 0, options: [.calculationModeLinear], animations: {
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
-                self.circle.frame.size.width = 80
-                self.circle.frame.origin.x += midOfNewPostion
-                self.circle.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                self.bubble.frame.size.width = 80
+                self.bubble.frame.origin.x += midOfNewPostion
+                self.bubble.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
             })
             UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
-                self.circle.transform = CGAffineTransform(scaleX: 1, y: 1)
-                self.circle.frame.origin.x += midOfNewPostion
-                self.circle.frame.size.width = 40
+                self.bubble.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.bubble.frame.origin.x += midOfNewPostion
+                self.bubble.frame.size.width = 40
 
             })
         }, completion: { _ in
